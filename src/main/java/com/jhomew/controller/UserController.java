13976514ba5d71deeddfcd642570b2e8b3268e9f
HkisY <lzc1112333666@126.com>
2021-10-13 09:38:08 +0800

    fix xml bug

--- src/main/java/com/jhomew/controller/UserController.java
+++ src/main/java/com/jhomew/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.jhomew.controller;
 
 
+import com.jhomew.entity.User;
+import com.jhomew.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -14,8 +20,17 @@ import org.springframework.stereotype.Controller;
  * @since 2021-10-10
  */
 @Controller
-@RequestMapping("//user")
 public class UserController {
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+    @Autowired
+    UserService service;
 
+    @RequestMapping("/list")
+    @ResponseBody
+    public User json(){
+        logger.debug("----------123-1-312312313123-----");
+        System.out.println("asdadsadsadada");
+        return service.getById(1);
+    }
 }
 
